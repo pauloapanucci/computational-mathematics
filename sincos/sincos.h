5eb763f662335ffbf1cbc66aa87cc37b99da6543
Tiago <xtheosirian@gmail.com>
2016-09-26 13:13:37 -0300

    fixed, now with a lot more precision

--- sincos/sincos.h
+++ sincos/sincos.h
@@ -1,21 +1,21 @@
 #include <math.h>
 
 double PI = 3.1415926535897932;
-double FA = 0.0174532925199432;
+double FA = 0.017453292519943295;
 
-double SA = 0.1666666666666666;
-double SB = 0.0083333333333333;
-double SC = 0.0001984126984126;
-double SD = 0.0000027557319223;
-double SE = 0.0000000250521083;
-double SF = 0.0000000001605904;
+double SA = 0.166666666666666666;
+double SB = 0.008333333333333333;
+double SC = 0.000198412698412698;
+double SD = 0.000002755731922398;
+double SE = 0.000000025052108385;
+double SF = 0.000000000160590438;
 
-double CA = 0.5;
-double CB = 0.0416666666666666;
-double CC = 0.0013888888888888;
-double CD = 0.0000248015873015;
-double CE = 0.0000002755731922;
-double CF = 0.0000000020876756;
+double CA = 0.500000000000000000;
+double CB = 0.041666666666666666;
+double CC = 0.001388888888888888;
+double CD = 0.000024801587301587;
+double CE = 0.000000275573192239;
+double CF = 0.000000002087675698;
 
 double fsin(double deg);
 double fcos(double deg);
@@ -41,21 +41,7 @@ double fsin(double deg){
     }
     else {
       double sqr = x * x;
-      double res;
-      res = SF * sqr;
-      res += SE;
-      res *= sqr;
-      res = SD - res;
-      res *= sqr;
-      res += SC;
-      res *= sqr;
-      res = SB - res;
-      res *= sqr;
-      res += SA;
-      res *= sqr;
-      res = 1 - res;
-      res *= x;
-      return res;
+      return x + (x * (sqr *( sqr*( sqr*( sqr*( sqr*(sqr*SF - SE) + SD) - SC) + SB) - SA)));
     }
   } else if (deg == 0) {
     return 0;
@@ -81,20 +67,7 @@ double fcos(double deg){
     if (deg > 45) return fsin(90 - deg);
     else {
       double sqr = x * x;
-      double res;
-      res = CF * sqr;
-      res += CE;
-      res *= sqr;
-      res = CD - res;
-      res *= sqr;
-      res += CC;
-      res *= sqr;
-      res = CB - res;
-      res *= sqr;
-      res += CA;
-      res *= sqr;
-      res = 1 - res;
-      return res;
+      return 1 + ( sqr*( sqr*( sqr*( sqr*( sqr*(CF*sqr - CE) + CD) - CC) + CB) - CA));
     }
   } else if (deg == 0) {
     return 1;
